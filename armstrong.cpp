b04c73582ee48505afd0cbefa85c44f6d9764cd6
SwAtz07 <47653704+SwAtz07@users.noreply.github.com>
2019-04-12 15:36:12 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -11,16 +11,22 @@ class Armstrong
 			a=a*a*a;
 			return a;
 		}
+		int p(int s)
+		{
+			s=s*s;
+			return s;
+		}
 };
 int main()
 {
 	Armstrong a1;
-	int no,a,num,nu,sum=0,k;
+	int no,a,num,nu,sum=0,k,n;
 	ofstream obj("NUM1.DAT",ios::out);
-	//cout<<"Enter the number:";
-	//cin>>no;
-	//obj<<no;
-	//obj.close();
+	ofstream arr("NUM.DAT",ios::out);
+	cout<<"Enter the number:";
+	cin>>n;
+	arr<<n;
+	arr.close();
 	ifstream obj1("NUM.DAT",ios::in);
 	//obj1.read((char*)&a1,sizeof(a1));
 	if(!obj1.eof())
@@ -46,7 +52,8 @@ int main()
 	if(sum==a)
 	{
 		cout<<"Armstrong";
-		obj<<sum<<"\n";
+		int q=a1.p(sum);
+		obj<<q<<"\n";
 	}
 	else
 		cout<<"\nNot Armstrong";
@@ -54,6 +61,3 @@ int main()
 	obj1.close();
 	return 0;
 }
-		
-		
-	
